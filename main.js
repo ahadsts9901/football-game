672f6c3433e0935d96575610b68cffb9d7589c67
Muhammad Ahad <120649081+ahadsts9901@users.noreply.github.com>
2023-06-07 21:34:00 +0500

    javascript started

---
+++ main.js
@@ -0,0 +1,8 @@
+let topLeftButton = document.getElementById("button-1")
+let bottomLeftButton = document.getElementById("button-2")
+let topButton = document.getElementById("button-3")
+let bottomButton = document.getElementById("button-4")
+let topRightButton = document.getElementById("button-5")
+let bottomRightButton = document.getElementById("button-6")
+
+let football = document.getElementsByName("football")
